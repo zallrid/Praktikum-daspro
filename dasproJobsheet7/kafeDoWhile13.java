944ef4b05c8942071cfceb85ca0a6317f8c4fb96
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-10-29 10:45:28 +0700

    kafe

---
+++ dasproJobsheet7/kafeDoWhile13.java
@@ -0,0 +1,33 @@
+package dasproJobsheet7;
+import java.util.Scanner;
+
+public class kafeDoWhile13 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int kopi,totalHarga, teh, roti, hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000;
+
+    do{
+        System.out.print("Masukkan nama pelanggan (ketil 'batal' untuk keluar): ");
+        String namaPelanggan = sc.nextLine();
+
+        if(namaPelanggan.equalsIgnoreCase("batal")){
+            System.out.println("Transaksi dibatalkan.");
+            break;
+
+        }
+        System.out.print("Jumlah kopi: ");
+        kopi = sc.nextInt();
+        System.out.print("Jumlah teh: ");
+        teh = sc.nextInt();
+        System.out.print("Jumlah roti: ");
+        roti = sc.nextInt();
+
+        totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+        System.out.println("Total yang harus dibayar: Rp" + totalHarga);
+        sc.nextLine();
+    }while (true);
+
+System.out.println("Semua transaksi selesai");
+}    
+}
