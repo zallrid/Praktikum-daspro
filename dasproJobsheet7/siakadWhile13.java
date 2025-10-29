64dc7c5b36f9975aae41f019cbe05479d8bed9c5
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-10-29 10:12:29 +0700

    modifikasiWhile

--- dasproJobsheet7/siakadWhile13.java
+++ dasproJobsheet7/siakadWhile13.java
@@ -24,6 +24,7 @@ public class siakadWhile13 {
 
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                System.out.println("Bagus, pertahankan nilainya");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
