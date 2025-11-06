fc07e3a08c41cd16b804f560953334de7a52a115
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-11-06 18:28:14 +0700

    latihan

---
+++ dasproJobsheet8/square13.java
@@ -0,0 +1,24 @@
+package dasproJobsheet8;
+import java.util.Scanner;
+
+public class square13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n = ");
+        int n = sc.nextInt();
+
+        for (int iOuter=1; iOuter <= n; iOuter++){
+        for (int i = 1; i <= n; i++){
+        System.out.print("*");
+
+    }
+    System.out.println();
+}
+}
+
+    
+    
+
+ {
+ }  
+ }
---
+++ dasproJobsheet8/star13.java
@@ -0,0 +1,14 @@
+package dasproJobsheet8;
+import java.util.Scanner;
+
+public class star13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n = ");
+            int n = sc.nextInt();
+    
+            for (int i = 1; i <= n; i++){
+            System.out.print("*");
+            }
+    }
+}
