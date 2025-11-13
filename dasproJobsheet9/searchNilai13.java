06abbdc73bd4aa3e5a418b5f941382943252ff98
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-11-13 23:40:28 +0700

    modifikasi pertanyaan 2, percobaan 4

--- dasproJobsheet9/searchNilai13.java
+++ dasproJobsheet9/searchNilai13.java
@@ -1,19 +1,41 @@
 package dasproJobsheet9;
 
+import java.util.Scanner;
+
 public class searchNilai13 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+        int key = 0;
         int hasil = 0;
+        int k = 0;
 
-        for ( int i = 0; i < arrNilai.length; i++){
-            if (key == arrNilai[i]){
-                hasil = i;
-                break;
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jmlNilai = sc.nextInt();
+
+        int[] arrNilai = new int[jmlNilai];
+        while (k < jmlNilai) {
+            for (int i = 0; i < arrNilai.length; i++) {
+                System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+                arrNilai[i] = sc.nextInt();
+
             }
+            System.out.println("--------------------------------------");
+            System.out.print("Masukkan nilai yang ingin dicari: ");
+            key = sc.nextInt();
+            
+
+            for (int i = 0; i < arrNilai.length; i++) {
+                if (key == arrNilai[i]) {
+                    hasil = i;
+                    System.out.println();
+                    System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil+1));
+                    break;
+                }
+            }
+           
+            System.out.println();
+
+            break;
         }
-        System.out.println();
-        System.out.println("Nilai "+ key+" ketemu di indeks ke-" +hasil);
-        System.out.println();
     }
 }
