259140bcf66f5feb6736da4f29782ae43a7e0d19
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-11-13 20:36:36 +0700

    percobaan 2

---
+++ dasproJobsheet9/arrayNilai13.java
@@ -0,0 +1,20 @@
+package dasproJobsheet9;
+
+import java.util.Scanner;
+
+public class arrayNilai13 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++){
+            System.out.print("Masukkan nilai akhir ke-" + i + "; ");
+            nilaiAkhir[i] = sc.nextInt();
+        } 
+            for (int i = 0; i < 10; i++){
+                System.out.println("Nilai akhir ke-" + i + " adalah "+ nilaiAkhir[i]);
+            }
+    }
+}
