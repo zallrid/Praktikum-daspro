d692dbda671ec321e261a139f9b92a37118a909b
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-11-13 21:44:05 +0700

    modifikasi

--- dasproJobsheet9/arrayNilai13.java
+++ dasproJobsheet9/arrayNilai13.java
@@ -9,12 +9,17 @@ public class arrayNilai13 {
 
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++){
-            System.out.print("Masukkan nilai akhir ke-" + i + "; ");
+        for (int i = 0; i < nilaiAkhir.length; i++){
+            System.out.print("Masukkan nilai akhir ke-" + i + ": ");
             nilaiAkhir[i] = sc.nextInt();
         } 
-            for (int i = 0; i < 10; i++){
-                System.out.println("Nilai akhir ke-" + i + " adalah "+ nilaiAkhir[i]);
+            for (int i = 0; i < nilaiAkhir.length; i++){
+                if (nilaiAkhir[i] > 70){
+                System.out.println("Mahasiswa ke-" + i + " lulus!!!");
+            }else {
+                System.out.println("Mahasiswa ke-"+ i + " tidak lulus :(");
             }
+
+        }
     }
 }
