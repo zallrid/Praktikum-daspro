e3736f62a981cb61df83d9dc88f5aa90c832bff1
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-11-19 10:25:08 +0700

    pertanyaan 9

--- dasproJobsheet10/bioskop13.java
+++ dasproJobsheet10/bioskop13.java
@@ -21,9 +21,9 @@ public class bioskop13 {
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
         
-System.out.println("Penonton pada baris ke-3: ");
-for (String i : penonton[2]) {
-System.out.println(i);
+System.out.println(penonton.length);
+for (int i = 0; i < penonton.length; i++) {
+System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
 
 
 
