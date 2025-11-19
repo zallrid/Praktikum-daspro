91b7664c466619fc107b81834d6e36c23d16dc38
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-11-19 10:17:47 +0700

    pertanyaan 6

---
+++ dasproJobsheet10/bioskop13.java
@@ -0,0 +1,33 @@
+package dasproJobsheet10;
+
+import java.util.Scanner;
+
+public class bioskop13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[][] penonton = new String[4][2];
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+
+        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        
+System.out.println(penonton.length);
+for (String[] barisPenonton : penonton) {
+System.out.println("Panjang baris: " + barisPenonton.length);
+
+
+
+
+    }
+}
+}
