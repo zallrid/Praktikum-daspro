5517a1aafd3575230ae4eb105d517180699e0e4d
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-11-21 21:21:46 +0700

    modifikasi pertanyaan 4 percobaan 3

--- dasproJobsheet10/numbers13.java
+++ dasproJobsheet10/numbers13.java
@@ -14,6 +14,9 @@ public class numbers13 {
     for (int i = 0; i < myNumbers.length; i++) {
       System.out.println(Arrays.toString(myNumbers[i]));
     }
+    for (int i = 0; i < myNumbers.length; i++) {
+     System.out.println("Panjang baris ke-" + (i+1) + " : " + myNumbers[i].length);
+      }
+    }
 
-}
 }
