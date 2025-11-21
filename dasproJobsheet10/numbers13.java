05cba8f60d481d6bca3bf81055f54622abec3857
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-11-21 21:16:03 +0700

    pertanyaan 1 percobaan 3

---
+++ dasproJobsheet10/numbers13.java
@@ -0,0 +1,19 @@
+package dasproJobsheet10;
+
+import java.util.Arrays;
+
+public class numbers13 {
+    public static void main(String[] args) {
+        
+    
+    int[][] myNumbers = new int[3][];
+    myNumbers[0] = new int [5];
+    myNumbers[1] = new int [3];
+    myNumbers[2] = new int [1];
+
+    for (int i = 0; i < myNumbers.length; i++) {
+      System.out.println(Arrays.toString(myNumbers[i]));
+    }
+
+}
+}
