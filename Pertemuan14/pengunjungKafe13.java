75a1dd24dd72ddb0e076c614c542d62d79750658
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-12-03 01:14:40 +0700

    modifikasi percobaan 4, pertanyaan 2

--- Pertemuan14/pengunjungKafe13.java
+++ Pertemuan14/pengunjungKafe13.java
@@ -3,8 +3,11 @@ package Pertemuan14;
 public class pengunjungKafe13 {
     static void daftarPengunjung(String...namaPengunjung){
         System.out.println("DAftar nama penguunjung: ");
-        for (int i = 0 ; i < namaPengunjung.length; i++) {
-            System.out.println("- "+namaPengunjung[i]);
+        // for (int i = 0 ; i < namaPengunjung.length; i++) {
+        //     System.out.println("- "+namaPengunjung[i]);
+        // }
+        for (String nama : namaPengunjung) {
+            System.out.println("- "+nama);
         }
     }
     public static void main(String[] args) {
