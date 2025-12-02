8cc7d225860b3614c4331c3eb6c3802c21b22dea
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-12-02 10:21:45 +0700

    percobaan 1

---
+++ Pertemuan14/kafe13.java
@@ -0,0 +1,19 @@
+package Pertemuan14;
+
+public static void main(String[] args) {
+        Menu();
+    }
+public class kafe13 {
+    public static void Menu() {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1, Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("==========================");
+        System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+
+}
