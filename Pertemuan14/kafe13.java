247353defb9f98a77c945efd01f1e431a0682f14
farid sigma gokilll <mohamadfaridrizaldhi@gmail.com>
2025-12-02 13:33:22 +0700

    modifikasi percobaan 2

--- Pertemuan14/kafe13.java
+++ Pertemuan14/kafe13.java
@@ -2,17 +2,26 @@ package Pertemuan14;
 
 public class kafe13 {
     public static void main(String[] args) {
-       
-        System.out.println("===== MENU RESTO KAFE =====");
-        System.out.println("1, Kopi Hitam - Rp 15,000");
-        System.out.println("2. Cappuccino - Rp 20,000");
-        System.out.println("3. Latte - Rp 22,000");
-        System.out.println("4. Teh Tarik - Rp 12,000");
-        System.out.println("5. Roti Bakar - Rp 10,000");
-        System.out.println("6. Mie Goreng - Rp 18,000");
-        System.out.println("==========================");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
+        Menu("Andi",true);
+    }
+       public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang di Resto Kafe, " + namaPelanggan + "!"   );
+        
+        if (isMember){
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+           System.out.println("===== MENU RESTO KAFE =====");
+           System.out.println("1, Kopi Hitam - Rp 15,000");
+           System.out.println("2. Cappuccino - Rp 20,000");
+           System.out.println("3. Latte - Rp 22,000");
+           System.out.println("4. Teh Tarik - Rp 12,000");
+           System.out.println("5. Roti Bakar - Rp 10,000");
+           System.out.println("6. Mie Goreng - Rp 18,000");
+           System.out.println("==========================");
+           System.out.println("Silakan pilih menu yang Anda inginkan.");
+       }
     }
    
     
-}
+
